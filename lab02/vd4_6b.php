f461b45b5293d34ef51274ec8e67cd7d6e9c4cb8
anna123-mmm <annaminh2708@gmail.com>
2025-10-25 11:39:08 +0700

    Add files via upload

---
+++ lab02/vd4_6b.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>lab 2_5</title>
+</head>
+
+<body>
+<?php
+	require("lab2_5a.php");
+	require("lab2_5b.php");
+	require("lab2_5b.php");
+
+	if(isset($x))
+		echo "Giá trị của x là: $x";
+	else
+		echo "Biến x không tồn tại";
+
+    echo "<br/> Vi require 5a ne da gan gia tri cho x=10 nen require them 5b se x=x+10";
+    echo "<br/> Require khac include o cho o include neu file .php khong ton tai, chi canh bao con require thi se dung chuong trinh ngay";
+?>
+</body>
+</html>
